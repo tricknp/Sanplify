76107427096d5eba3198f2153c31b90b93bc7bf0
tricknp <np_patrick@yahoo.com>
2018-07-31 19:19:50 -0300

    update

---
+++ src/components/MainMenu/SetLocation.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="set-location-box">
+    <div class="set-location-container">
+
+      <div class="set-location-header">
+        <div class="radio-location-header">
+          <svg class="location-header-icon" enable-background="new 0 0 306.773 306.773" version="1.1" viewBox="0 0 306.773 306.773" xml:space="preserve" xmlns="http://www.w3.org/2000/svg">
+	           <path d="m302.93 149.79c5.561-6.116 5.024-15.49-1.199-20.932l-137.1-119.96c-6.223-5.442-16.2-5.328-22.292 0.257l-137.57 126.1c-6.092 5.585-6.391 14.947-0.662 20.902l3.449 3.592c5.722 5.955 14.971 6.665 20.645 1.581l10.281-9.207v134.79c0 8.27 6.701 14.965 14.965 14.965h53.624c8.264 0 14.965-6.695 14.965-14.965v-94.3h68.398v94.3c-0.119 8.264 5.794 14.959 14.058 14.959h56.828c8.264 0 14.965-6.695 14.965-14.965v-132.89s2.84 2.488 6.343 5.567c3.497 3.073 10.842 0.609 16.403-5.513l3.897-4.284z" />
+          </svg>
+        </div>
+        <h4 class="location-header-title">Defina um endereço residencial.</h4>
+      </div>
+
+      <form class="set-location-form">
+        <input class="location-form-input" type="text">
+        <button class="location-form-btn" type="submit">Definir</button>
+      </form>
+
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+
+}
+</script>
