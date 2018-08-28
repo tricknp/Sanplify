0495ff96d78680d59576938f34b0088f9c106523
tricknp <np_patrick@yahoo.com>
2018-08-27 22:59:01 -0300

    update layout from login

--- src/components/authentication/Login.vue
+++ src/components/authentication/Login.vue
@@ -1,12 +1,19 @@
 <template>
-  <section>
-    <form class="login-container">
-      <input type="text" placeholder="username"     v-model="email">
-      <input type="password" placeholder="password" v-model="password">
-      <button type="button" @click="signIn()"> Entrar </button>
-      <h1> {{ `${email} ${password} ` }} </h1>
+  <section class="login">
+    <img src="../../../static/sanplifyLogo.svg" class="login__logo">
+    <h1 class="login__text">Seja bem vindo</h1>
 
+    <form class="login__form">
+      <input type="text" placeholder="Nome de usuario" v-model="email" class="login__form--input">
+      <input type="password" placeholder="Senha" v-model="password" class="login__form--input">
+
+      <button type="button" @click="signIn()" class="login__form--btn"> Entrar </button>
+      <span class="forgotPass">
+        Esqueceu sua senha?
+        <a class="forgotPass__action"> Clique aqui </a>
+      </span>
     </form>
+
   </section>
 </template>
 
